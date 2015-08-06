dc1bf5da15b97257ad4a7f84f53f2f75336034a8
Nils Blum-Oeste <mail@nils-blum-oeste.net>
2015-08-06 09:30:02 +0200

    Inital project setup

---
+++ README.md
@@ -0,0 +1,9 @@
+# clj-tunes
+
+An Overtone demo project for Clojure Dojo learning groups in Berlin
+
+## License
+
+GNU Affero General Public License v3
+
+![AGPLv3](https://gnu.org/graphics/agplv3-155x51.png)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-tunes "0.1.0-SNAPSHOT"
+  :description "An Overtone demo project for Clojure Dojo learning
+  groups in Berlin"
+  :url "https://github.com/nblumoe/clj-tunes"
+  :license {:name "GNU Affero General Public License"
+            :url "https://gnu.org/licenses/agpl.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
