217ab4d6e7c5552abfb57e7be12d1346212fae7a
Nils Blum-Oeste <mail@nils-blum-oeste.net>
2015-08-15 19:22:26 +0200

    Extract tapping to own namespace

--- src/clj_tunes/oscilliscope.clj
+++ src/clj_tunes/oscilliscope.clj
@@ -1,6 +1,6 @@
 (ns clj-tunes.oscilliscope
   (:require [quil.core :as q]
-            [quil.applet]))
+            [clj-tunes.tap :as tap]))
 
 ;; for whatever reason, we need to create a first sketch before
 ;; starting overtone, otherwise an exception is thrown from time to
@@ -11,63 +11,25 @@
 
 (use 'overtone.live)
 
-(def ^:private
-  ^:dynamic
-  *taps* nil)
-
-(def sampled-wave (atom [[0 0]]))
-
-(defn- clear-osc! [] (reset! sampled-wave [[0 0]]))
-
-(defn- store-osc-sample!
-  "Updates the sampled-wave atom by appending the given value. The
-  time variable for the appended data point is the timestamp of the
-  last point in the existing vectore plus dt."
-  [value dt]
-  (swap! sampled-wave #(conj % [(+ dt (first (last %)))
-                                value])))
-
-(defsynth tapper
-  "A synth to tap the stereo output bus."
-  [bus 0 freq 300]
-  (tap :stereo-out freq (in:ar bus)))
-
-(defn start-tapping []
-  (let [tapper (tapper)
-        taps (:taps tapper)
-        freq (get-in tapper [:args "freq"])
-        dt (/ 1 freq)]
-    ;; current value of the output bus can be retrieved via get-taps
-    (alter-var-root (var *taps*) (constantly taps))
-    ;; on every sample tap event, append time and amplitude to an atom
-    (on-event "/overtone/tap" (fn [{[_ _ value] :args}]
-                                (store-osc-sample! value dt))
-              ::tapper-listener)))
-
-(defn get-taps
-  "Deref and return all of our taps as a plain old map."
-  []
-  (when *taps* (into {} (map (fn [[k v]] [k @v]) *taps*))))
+(defn- setup []
+  (q/smooth)
+  (q/frame-rate 30)
+  (q/background 0)
+  (q/stroke 0 255 0)
+  (q/fill 0 255 0))
 
 (defn create! [& {:keys [width height max-time max-amplitude]
                   :or {width 600
                        height 300
-                       max-time 3
+                       max-time 0.1
                        max-amplitude 2}}]
   (let [time-scale (/ width max-time)
         amp-scale (/ height 2 max-amplitude)]
 
-    (defn- setup []
-      (q/smooth)
-      (q/frame-rate 30)
-      (q/background 0)
-      (q/stroke 0 255 0)
-      (q/fill 0 255 0))
-
     (defn- draw []
       (q/background 0)
       (q/with-translation [0 (/ (q/height) 2)]
-        (loop [points @sampled-wave]
+        (loop [points (tap/get-buffered-taps)]
           (when (next points)
             (let [[x1 y1] (first points)
                   [x2 y2] (second points)]
@@ -79,12 +41,14 @@
       :size [width height]
       :draw draw
       :setup setup
-      :display 1)))
+      :display 0)))
 
 (defn demo-osc
   []
   (stop)
-  (clear-osc!)
   (create!)
-  (demo 2 (sin-osc 3))
-  (start-tapping))
+  (tap/clear-buffer!)
+  (demo 1 (sin-osc 300))
+  (tap/start-tapping))
+
+(comment (demo-osc))
---
+++ src/clj_tunes/tap.clj
@@ -0,0 +1,52 @@
+(ns clj-tunes.tap
+  (:require [overtone.core :refer :all]))
+
+(def ^:private ^:dynamic *taps* nil)
+
+(def ^:private tap-buffer (atom [[0 0]]))
+
+(defn- store-to-buffer!
+  "Updates the tap buffer atom by appending the given value. The
+  time variable for the appended data point is the timestamp of the
+  last point in the existing vectore plus dt."
+  [value dt]
+  (swap! tap-buffer #(conj % [(+ dt (first (last %)))
+                                value])))
+
+(defn- register-tap-buffer
+  [active? dt]
+  (if active?
+    (on-event
+     "/overtone/tap"
+     (fn [{[_ _ value] :args}]
+       (store-to-buffer! value dt))
+     ::tapper-listener)
+    (remove-event-handler ::tapper-listener)))
+
+(defsynth tapper
+  "A synth to tap the stereo output bus."
+  [bus 0 freq 500]
+  (tap :stereo-out freq (in:ar bus)))
+
+(defn start-tapping
+  [& {:keys [buffered?]}]
+  (let [tapper (tapper)
+        taps (:taps tapper)
+        freq (get-in tapper [:args "freq"])
+        dt (/ 1 freq)]
+    (println "---" buffered?)
+    (register-tap-buffer buffered? dt)
+    ;; current value of the output bus can be retrieved via get-taps
+    (alter-var-root (var *taps*) (constantly taps))))
+
+(defn get-taps
+  "Deref and return all of our taps as a plain old map."
+  []
+  (when *taps* (into {} (map (fn [[k v]] [k @v]) *taps*))))
+
+(defn get-buffered-taps [] @tap-buffer)
+
+(defn clear-buffer!
+  "Resets the buffered tap data."
+  []
+  (reset! tap-buffer [[0 0]]))
