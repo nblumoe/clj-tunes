f7dc1e111698ca9f6bc3916a6dbe599e1b141c3a
Nils Blum-Oeste <mail@nils-blum-oeste.net>
2015-08-16 10:10:25 +0200

    Split oscilliscope into more functions

--- src/clj_tunes/oscilliscope.clj
+++ src/clj_tunes/oscilliscope.clj
@@ -10,37 +10,30 @@
   (q/stroke 0 255 0)
   (q/fill 0 255 0))
 
-(defn create! [& {:keys [width height max-time max-amplitude]
-                  :or {width 600
-                       height 300
-                       max-time 1
-                       max-amplitude 2}}]
-  (let [time-scale (/ width max-time)
-        amp-scale (/ height 2 max-amplitude)]
-
-    (defn- draw []
-      (q/background 0)
-      (q/with-translation [0 (/ (q/height) 2)]
-        (loop [points (tap/get-buffered-taps)]
-          (when (next points)
-            (let [[x1 y1] (first points)
-                  [x2 y2] (second points)]
-              (q/line (* time-scale x1) (* amp-scale y1)
-                      (* time-scale x2) (* amp-scale y2))
-              (recur (next points)))))))
-
-    (q/defsketch osc-plot
-      :size [width height]
-      :draw draw
-      :setup setup
-      :display 0)))
+(defn- oscilloscope
+  [& {:keys [max-time max-amplitude]
+      :or {max-time 1
+           max-amplitude 2}}]
+  (let [time-scale (/ (q/width) max-time)
+        amp-scale (/ (q/height) 2 max-amplitude)]
+    (q/background 0)
+    (q/with-translation [0 (/ (q/height) 2)]
+      (loop [points (tap/get-buffered-taps)]
+        (when (next points)
+          (let [[x1 y1] (first points)
+                [x2 y2] (second points)]
+            (q/line (* time-scale x1) (* amp-scale y1)
+                    (* time-scale x2) (* amp-scale y2))
+            (recur (next points))))))))
 
 ;; TODO allow plotting multiple oscillators
-(defn plot-osc
-  [duration synth & synth-args]
+(defn plot-osc [duration synth & synth-args]
+  (q/defsketch osc-plot
+    :size [600 600]
+    :draw (fn [] (oscilloscope :max-time duration))
+    :setup setup
+    :display 1)
   (stop)
-  (create! :max-time duration)
   (tap/clear-buffer!)
   (apply synth synth-args)
-  (tap/start-tapping :buffered? true
-                     :freq 300))
+  (tap/start-tapping :buffered? true :freq 300))
