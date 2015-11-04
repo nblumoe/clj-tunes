93964b212ef57f98f7f51b0c15b5f77c39daaa97
Tamara Atanasoska <tamara.atanasoska20@gmail.com>
2015-11-04 22:20:55 +0100

    Refactor tap-sound, change frequency of tap

--- src/clj_tunes/oscilloscope.clj
+++ src/clj_tunes/oscilloscope.clj
@@ -14,8 +14,8 @@
   [& {:keys [max-time max-amplitude]
       :or {max-time 1
            max-amplitude 2}}]
-  (let [time-scale (/ (q/width) max-time)
-        amp-scale (/ (q/height) 2 max-amplitude)]
+  (let [time-scale (/ (* 3 (q/width)) max-time)
+        amp-scale (/ (* 2 (q/height)) 2 max-amplitude)]
     (q/background 0)
     (q/with-translation [0 (/ (q/height) 2)]
       (loop [points (tap/get-buffered-taps)]
@@ -23,7 +23,8 @@
           (let [[x1 y1] (first points)
                 [x2 y2] (second points)]
             (q/line (* time-scale x1) (* amp-scale y1)
-                    (* time-scale x2) (* amp-scale y2))
+                    (* time-scale x2) (* amp-scale y2)) ;try flipping the y value (negating it)
+                                                        ; try switching x and y 
             (recur (next points))))))))
 
 ;; TODO allow plotting multiple oscillators
@@ -32,12 +33,27 @@
     :size [600 600]
     :draw (fn [] (oscilloscope :max-time duration))
     :setup setup
-    :display 1)
+    :display 0)
   (stop)
   (tap/clear-buffer!))
 
-(defn tap-sound [duration sample-path]
+;(def sound-path (sample "/home/tamara/Desktop/activity_unproductive.wav"))
+;(defn tap-sound [duration sound]
+;  (plot-osc duration)
+;  (sound)
+;  (tap/start-tapping :buffered? true :freq 300))
+
+(defn tap-sound [duration sound-path]
   (plot-osc duration)
-  (sample sample-path)
-  (tap/start-tapping :buffered? true :freq 300))
+  (let [sound-map (sample sound-path)]
+    (sound-map))
+  (tap/start-tapping :buffered? true :freq 20))
+
+(def sample-buf (load-sample "/home/tamara/Desktop/activity_unproductive.wav"))
+
+(defsynth reverb-on-left []
+  (let [dry (play-buf 1 sample-buf)
+    wet (free-verb dry 1)]
+    (out 0 [wet dry])))
+
 
